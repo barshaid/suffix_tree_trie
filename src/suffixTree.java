983f71776fdb414a1c67d516756825e76e7f068a
barshaid <exofang@hotmail.com>
2019-12-22 23:58:31 +0200

    nothing really

--- src/suffixTree.java
+++ src/suffixTree.java
@@ -1,4 +1,9 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
-public class suffixTree {
+public class suffixTree{
+ 
 
 }
--- src/suffixTrie.java
+++ src/suffixTrie.java
@@ -1,4 +1,41 @@
-
 public class suffixTrie {
+	static final int d = 4;
 
+	static class TrieNode {
+		TrieNode[] children = new TrieNode[d];;
+		char key;
+
+		TrieNode() {
+			
+
+			for (int i = 0; i < d; i++)
+				children[i] = null;
+		}
+	};
+
+	TrieNode root;
+
+	suffixTrie(String s) {
+		s += "$";
+		root = new TrieNode();
+		insert(s, 0, root);
+
+	}
+
+	void insert(String s, int i, TrieNode node) {
+			 String suffix[]=new String[s.length()];
+			
+		   }
+	
+	
+	TrieNode followPath(String s, TrieNode curr) {
+		if(s.charAt(0)!=curr.key) 
+			return null;
+		curr=curr
+		return curr;
+	}
+
+	void print() {
+
+	}
 }
